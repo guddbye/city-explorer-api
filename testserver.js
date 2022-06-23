ab29c53bf5eb0060d4ef5544359b78bf74091722
Brentice Loper <54426613+guddbye@users.noreply.github.com>
2022-06-23 00:00:59 -0700

    Made something.

--- testserver.js
+++ testserver.js
@@ -1,26 +1,29 @@
 'use strict';
 
+console.log('Server face.');
 const express = require('express');
 require('dotenv').config();
+const weatherData = require('./data/weather.json');
+
 const app = express();
 
-app.use(express.urlencoded({ extended: false}));
+const PORT=process.env.PORTANDRE || 3002;
 
-app.get('/', (req, res) => {
-  res.send('/');
-});
-
-app.get('*', (req, res)=>{
-  res.send('The page you are looking for doesn\'t exist');
-});
-
-function getWeather(req, res, next) {
-req.visitorWeather = true;
-next();
-
-}
-app.get ('/getWeather', (req, res) => {
+app.get('/', (request,response)=>{
+  response.send('Hello from the other side!');
 
 });
 
-app.listen(3000);
+app.get('/weather',(request,response) => {
+
+  let dataToSend = weatherData.find(data => data.city_name === request.query.city);
+  response.send(dataToSend);
+  response.send('test of weather route');
+});
+
+app.get('*', (request,response) => {
+  response.send('There seems to be an error.');
+});
+
+app.listen(PORT,() => console.log(`PORT: ${PORT}`));
+
